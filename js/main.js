2b4cf6c6ce0f0271350ae5a3622e9ee05434e62c
nirajmdr <niraj_mdr@outlook.com>
2021-05-02 15:09:42 +0545

    Final

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// var preloader=document.getElementById('overlayer');
+// function myFunction(){
+//   preloader.style.display='none';
+// }
+
+setTimeout(function(){
+$('#overlayer').fadeToggle();
+},4000);
+
+setTimeout(function(){
+  $('.elegent').fadeToggle();
+  },1500);
+
+  setTimeout(function(){
+    $('.you').fadeToggle();
+    },2500);
